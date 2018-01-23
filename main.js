c64575d9f54a7b1281e1817028072ed0715bfb58
Vladan <vladanvelojic@gmail.com>
2018-01-23 21:18:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+anime({
+    targets: '.floating-images .float',
+    translateY: [
+        {value: 15, duration: 3500},
+        {value: 0, duration: 2000}
+    ],
+    easing: 'easeInOutQuart',
+    loop: true,
+});
